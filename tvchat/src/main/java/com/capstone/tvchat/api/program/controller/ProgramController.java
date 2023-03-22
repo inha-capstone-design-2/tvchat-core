868259e26120f29551a3af43b2285653047ce60e
skysrd <skysrd@gmail.com>
2023-03-23 03:15:00 +0900

    [ADD] Program Entity, Controller

---
+++ tvchat/src/main/java/com/capstone/tvchat/api/program/controller/ProgramController.java
@@ -0,0 +1,42 @@
+package com.capstone.tvchat.api.program.controller;
+
+import com.capstone.tvchat.common.BaseEntity.JsonResultData;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.spring.web.json.Json;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/program")
+@Api("프로그램 API Controller")
+public class ProgramController {
+    private final ProgramService programService;
+
+    @ApiOperation(value = "프로그램 전체 조회 API")
+    @GetMapping("/")
+    public ResponseEntity<?> getAllPrograms() {
+        return ResponseEntity.ok(
+                JsonResultData.successResultBuilder()
+                        .data(programService.getAllPrograms())
+                        .build()
+        );
+    }
+
+    @ApiOperation(value = "채널 별 프로그램 조회 API")
+    @GetMapping("/{channel-id}")
+    public ResponseEntity<?> getProgramByChannelId(@RequestParam("channel-id") Long channelId) {
+        return ResponseEntity.ok(
+                JsonResultData.successResultBuilder()
+                        .data(programService.getProgramByChannelId(channelId))
+                        .build()
+        );
+    }
+
+
+}
---
+++ tvchat/src/main/java/com/capstone/tvchat/api/program/domain/entity/Program.java
@@ -0,0 +1,37 @@
+package com.capstone.tvchat.api.program.domain.entity;
+
+import com.capstone.tvchat.api.channel.domain.entity.Channel;
+import com.capstone.tvchat.common.BaseEntity.BaseEntity;
+import com.fasterxml.jackson.databind.ser.Serializers;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Table(name = "PROGRAM")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Program extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "program_id")
+    private Long id;
+
+    @Column(name = "program_title")
+    private String title;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "channel_id")
+    private Channel channel;
+
+    @Builder
+    public Program(Long id, String title, Channel channel) {
+        this.id = id;
+        this.title = title;
+        this.channel = channel;
+    }
+}
