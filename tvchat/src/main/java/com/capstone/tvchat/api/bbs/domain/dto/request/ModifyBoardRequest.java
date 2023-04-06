a5faedc1f9b59d09d874cdf32f252153f16ee600
skysrd <skysrd@gmail.com>
2023-04-06 14:58:01 +0900

    [PATCH] ModifyBoardRequest

--- tvchat/src/main/java/com/capstone/tvchat/api/bbs/domain/dto/request/ModifyBoardRequest.java
+++ tvchat/src/main/java/com/capstone/tvchat/api/bbs/domain/dto/request/ModifyBoardRequest.java
@@ -1,6 +1,7 @@
 package com.capstone.tvchat.api.bbs.domain.dto.request;
 
 import com.capstone.tvchat.api.bbs.domain.entity.Board;
+import com.capstone.tvchat.api.program.domain.entity.Program;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,7 +13,7 @@ public class ModifyBoardRequest {
     private String imagePath;
     private String description;
 
-    public void modifyBoard(Board board, ModifyBoardRequest modifyBoardRequest) {
-        board.modifyBoard(modifyBoardRequest.getBoardName(), modifyBoardRequest.);
+    public void modifyBoard(Board board, ModifyBoardRequest modifyBoardRequest, Program program) {
+        board.modifyBoard(modifyBoardRequest.getBoardName(), program, modifyBoardRequest.getImagePath(), modifyBoardRequest.getDescription());
     }
 }
