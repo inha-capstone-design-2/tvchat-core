9ec26dedf8d174bac19c18208736d868de8f6727
skysrd <skysrd@gmail.com>
2023-04-26 04:35:24 +0900

    [ADD] GetBookmark

--- tvchat/src/main/java/com/capstone/tvchat/api/bookmark/controller/BookmarkController.java
+++ tvchat/src/main/java/com/capstone/tvchat/api/bookmark/controller/BookmarkController.java
@@ -2,12 +2,17 @@ package com.capstone.tvchat.api.bookmark.controller;
 
 import com.capstone.tvchat.api.bookmark.domain.dto.request.CreateBookmarkRequest;
 import com.capstone.tvchat.api.bookmark.service.BookmarkService;
+import com.capstone.tvchat.api.member.domain.enumerate.Authority;
 import com.capstone.tvchat.common.domain.JsonResultData;
+import com.capstone.tvchat.common.result.ResponseHandler;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -37,4 +42,14 @@ public class BookmarkController {
         );
     }
 
+    @ApiOperation("Bookmark 조회 API")
+    @GetMapping("/")
+    public ResponseEntity<?> getBookmark() {
+        return ResponseHandler.generate()
+                .data(bookmarkService.getBookmark(
+                        SecurityContextHolder.getContext().getAuthentication().getName()
+                ))
+                .status(HttpStatus.OK)
+                .build();
+    }
 }
